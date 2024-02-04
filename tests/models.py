5a5e86d5c395430e8deb137b69e06c6f1e3db4c4
Rustam <kodzokoff2016@yandex.ru>
2024-02-04 20:17:16 +0300

    некоторые изменения

--- tests/models.py
+++ tests/models.py
@@ -11,40 +11,32 @@ class Product:
         self.quantity = quantity
 
     def check_quantity(self, quantity) -> bool:
+        return self.quantity >= quantity
 
-        raise NotImplementedError
 
     def buy(self, quantity):
-        """
-        TODO реализуйте метод покупки
-            Проверьте количество продукта используя метод check_quantity
-            Если продуктов не хватает, то выбросите исключение ValueError
-        """
-        raise NotImplementedError
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError('Недостаточно товара на складе')
 
     def __hash__(self):
         return hash(self.name + self.description)
 
 
 class Cart:
-    """
-    Класс корзины. В нем хранятся продукты, которые пользователь хочет купить.
-    TODO реализуйте все методы класса
-    """
 
-    # Словарь продуктов и их количество в корзине
     products: dict[Product, int]
 
     def __init__(self):
-        # По-умолчанию корзина пустая
         self.products = {}
 
     def add_product(self, product: Product, buy_count=1):
-        """
-        Метод добавления продукта в корзину.
-        Если продукт уже есть в корзине, то увеличиваем количество
-        """
-        raise NotImplementedError
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
+
 
     def remove_product(self, product: Product, remove_count=None):
         """
@@ -52,6 +44,10 @@ class Cart:
         Если remove_count не передан, то удаляется вся позиция
         Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
         """
+        if remove_count > self.products[product]:
+            del self.products[product]
+        else:
+
         raise NotImplementedError
 
     def clear(self):
